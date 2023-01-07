901c2da32e9a27524b176e13ae061933fc0a6ef5
hongry18 <hongry18@gmail.com>
2023-01-07 13:06:08 +0900

    119.-Pascals-Triangle-II

---
+++ go/easy/119.-Pascals-Triangle-II/main.go
@@ -0,0 +1,14 @@
+package main
+
+func getRow(rowIndex int) []int {
+	result := make([]int, rowIndex+1)
+	result[0] = 1
+	for i := 0; i <= rowIndex; i++ {
+		result[i] = 1
+		for j := i - 1; j > 0; j-- {
+			result[j] = result[j] + result[j-1]
+		}
+	}
+
+	return result
+}
---
+++ go/easy/119.-Pascals-Triangle-II/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXxx(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		assert.Equal(t, getRow(5), []int{1, 3, 3, 1})
+	})
+}
