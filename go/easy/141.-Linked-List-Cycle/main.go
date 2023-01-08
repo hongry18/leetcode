b7220663e7b4b1c4e25ea008f87a191edb8766ac
hongry18 <hongry18@gmail.com>
2023-01-08 12:39:07 +0900

    141.-Linked-List-Cycle

--- go/easy/141.-Linked-List-Cycle/main.go
+++ go/easy/141.-Linked-List-Cycle/main.go
@@ -14,6 +14,10 @@ func hasCycle(head *ListNode) bool {
 	fast := head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
 	}
 	return false
 }
