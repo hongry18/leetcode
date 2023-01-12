fecc5437c45c9cf7b5bdfed637247dbad93ef673
hongry18 <hongry18@gmail.com>
2023-01-12 22:08:53 +0900

    160. Intersection of Two Linked Lists

---
+++ go/easy/160.-Intersection-of-Two-Linked-Lists/main.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a, b := headA, headB
+
+	for a != b {
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+	}
+
+	return a
+}
