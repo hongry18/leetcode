db99e400a3718dafeba58f1b99d74167af297d68
hongry18 <hongry18@gmail.com>
2023-01-13 13:35:16 +0900

    190. Reverse Bits - best practice

--- go/easy/190.-Reverse-Bits/main.go
+++ go/easy/190.-Reverse-Bits/main.go
@@ -1,6 +1,18 @@
 package main
 
+// best practice
 func reverseBits(num uint32) uint32 {
+	var res uint32
+	n := 31
+	for n > -1 {
+		res += num & 1 << n
+		num >>= 1
+		n--
+	}
+	return res
+}
+
+func reverseBits2(num uint32) uint32 {
 	var ar []uint32
 	for num > 0 {
 		ar = append(ar, num&1)
