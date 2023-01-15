753ea0eeb7b5446a9969e8aff5378d3c7a15244e
hongry18 <hongry18@gmail.com>
2023-01-15 18:04:02 +0900

    228. Summary Ranges

--- go/easy/228.-Summary-Ranges/main.go
+++ go/easy/228.-Summary-Ranges/main.go
@@ -1,11 +1,37 @@
 package main
 
+import "fmt"
+
 func summaryRanges(nums []int) []string {
 	var res []string
 
-	v := nums[0]
-	for i := 1; i < len(nums); i++ {
-		v = nums[i]
+	size := len(nums)
+	if size < 1 {
+		return res
+	}
+
+	if size == 1 {
+		res = append(res, fmt.Sprintf("%d", nums[0]))
+		return res
+	}
+
+	s := nums[0]
+	for i := 0; i < size-1; i++ {
+		if nums[i+1]-nums[i] != 1 {
+			if nums[i] == s {
+				res = append(res, fmt.Sprintf("%d", nums[i]))
+			} else {
+				res = append(res, fmt.Sprintf("%d->%d", s, nums[i]))
+			}
+
+			s = nums[i+1]
+		}
+	}
+
+	if s == nums[size-1] {
+		res = append(res, fmt.Sprintf("%d", nums[size-1]))
+	} else {
+		res = append(res, fmt.Sprintf("%d->%d", s, nums[size-1]))
 	}
 
 	return res
--- go/easy/228.-Summary-Ranges/main_test.go
+++ go/easy/228.-Summary-Ranges/main_test.go
@@ -10,4 +10,14 @@ func TestXxx(t *testing.T) {
 	t.Run("test1", func(t *testing.T) {
 		assert.Equal(t, summaryRanges([]int{0, 1, 2, 4, 5, 7}), []string{"0->2", "4->5", "7"})
 	})
+
+	t.Run("test2", func(t *testing.T) {
+		assert.Equal(t, summaryRanges([]int{0, 2, 4, 5, 6}), []string{"0", "2", "4->6"})
+	})
+
+	t.Run("test3", func(t *testing.T) {
+		assert.Equal(t, summaryRanges([]int{1}), []string{"1"})
+		assert.Equal(t, summaryRanges([]int{1, 2}), []string{"1->2"})
+		assert.Equal(t, summaryRanges([]int{1, 3}), []string{"1", "3"})
+	})
 }
