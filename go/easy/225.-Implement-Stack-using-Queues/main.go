170632e60c6adc85c0da3a335670cc2c34f63558
hongry18 <hongry18@gmail.com>
2023-01-16 11:32:38 +0900

    225. Implement Stack using Queues init

---
+++ go/easy/225.-Implement-Stack-using-Queues/main.go
@@ -0,0 +1,33 @@
+package main
+
+type MyStack struct {
+	ar []int
+}
+
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+func (this *MyStack) Push(x int) {
+}
+
+func (this *MyStack) Pop() int {
+	return 0
+}
+
+func (this *MyStack) Top() int {
+	return 0
+}
+
+func (this *MyStack) Empty() bool {
+	return false
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
