a4b1e0dd3098ded6e40867532ab5aaa1f0a741be
hongry18 <hongry18@gmail.com>
2023-01-18 09:18:18 +0900

    303. Range Sum Query - Immutable
    배열의 수들을 미리 합한 후 해당 범위를 연산 해 반환
    
    index start, end
    
    start == 0
      arr[end]
    start != 0
      arr[end] - arr[start-1]
    
    start 가 0일때는 end까지 기연산된 배열에서 요소를 가져오면 된다
    0이 아닐때 end의 요소에서 start - 1을 빼 주면 성립한다

--- go/easy/303.-Range-Sum-Query-Immutable/main.go
+++ go/easy/303.-Range-Sum-Query-Immutable/main.go
@@ -1,19 +1,27 @@
 package main
 
 type NumArray struct {
-	Data []int
+	size        int
+	prefixTable []int
 }
 
 func Constructor(n []int) NumArray {
-	return NumArray{Data: n}
+	arr := NumArray{size: len(n)}
+
+	arr.prefixTable = make([]int, arr.size)
+
+	arr.prefixTable[0] = n[0]
+	for i := 1; i < arr.size; i++ {
+		arr.prefixTable[i] = arr.prefixTable[i-1] + n[i]
+	}
+
+	return arr
 }
 
 func (this *NumArray) SumRange(left, right int) int {
-	var sum int
-
-	for i := left; i <= right; i++ {
-		sum += this.Data[i]
+	if left == 0 {
+		return this.prefixTable[right]
 	}
 
-	return sum
+	return this.prefixTable[right] - this.prefixTable[left-1]
 }
