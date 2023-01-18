820f3429f5860e6f29023677fa44962f652fbfb2
hongry18 <hongry18@gmail.com>
2023-01-18 10:45:09 +0900

    불필요한 컨디션 체크 제거

--- go/easy/345.-Reverse-Vowels-of-a-String/main.go
+++ go/easy/345.-Reverse-Vowels-of-a-String/main.go
@@ -2,23 +2,22 @@ package main
 
 func reverseVowels(s string) string {
 	b := []byte(s)
-	var find bool
+
 	for i, j := 0, len(b)-1; i < j; {
-		if find {
-			if match(b[j]) {
-				b[i], b[j] = b[j], b[i]
-				find = false
-				i++
-			}
-			j--
-		} else {
-			if match(b[i]) {
-				find = true
-			} else {
-				i++
-			}
+		if !match(b[i]) {
+			i++
+			continue
 		}
+		if !match(b[j]) {
+			j--
+			continue
+		}
+
+		b[i], b[j] = b[j], b[i]
+		i++
+		j--
 	}
+
 	return string(b)
 }
 
