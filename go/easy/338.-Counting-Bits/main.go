82a9c3a7fdabf24335eab9c4c5243b232a574e69
hongry18 <hongry18@gmail.com>
2023-01-18 09:59:07 +0900

    338. Counting Bits dp

--- go/easy/338.-Counting-Bits/main.go
+++ go/easy/338.-Counting-Bits/main.go
@@ -3,8 +3,12 @@ package main
 func countBits(n int) []int {
 	res := make([]int, n+1)
 
-	for i := 0; i <= n; i++ {
-		res[i] = res[i>>1] + i&1
+	offset := 1
+	for i := 1; i <= n; i++ {
+		if offset*2 == i {
+			offset = i
+		}
+		res[i] = 1 + res[i-offset]
 	}
 
 	return res
