c5f2ae62a70391725887514fa4223eff3ee81465
hongry <hongry@hongryui-MacBookPro.local>
2023-01-21 00:49:28 +0900

    optimize

--- go/easy/392.Is-Subsequence/main.go
+++ go/easy/392.Is-Subsequence/main.go
@@ -1,17 +1,11 @@
 package main
 
 func isSubsequence(s string, t string) bool {
-	if len(s) > len(t) {
-		return false
-	}
-	var i, j int
 
-	for i < len(s) && j < len(t) {
-		if s[i] == t[j] {
+	var i int
+	for j := 0; j < len(t); j++ {
+		if i < len(s) && s[i] == t[j] {
 			i++
-			j++
-		} else {
-			j++
 		}
 	}
 
