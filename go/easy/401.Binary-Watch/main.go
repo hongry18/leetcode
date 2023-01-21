573db6e022f3e0fca70006e4e6c319fd97e40464
hongry <hongry@hongryui-MacBookPro.local>
2023-01-21 14:43:59 +0900

    401. Binary Watch 풀이 보고도 모르겠음

--- go/easy/401.Binary-Watch/main.go
+++ go/easy/401.Binary-Watch/main.go
@@ -3,46 +3,25 @@ package main
 import "fmt"
 
 func readBinaryWatch(turnedOn int) []string {
-	if turnedOn > 8 {
-		return []string{}
-	}
-	if turnedOn == 0 {
-		return []string{"0:00"}
-	}
-	data := []int{1, 2, 4, 8, 16, 32, 101, 102, 104, 108}
-	visit := make([]bool, 10)
 	var res []string
-	comb(data, visit, 0, 10, turnedOn, &res)
+	if turnedOn > 8 {
+		return res
+	}
+	var ref = [60]int{}
+	for i := 1; i < 60; i++ {
+		ref[i] = ref[i>>1] + (i & 1)
+	}
 
-	return res
-}
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 60; j++ {
+			b := ref[i] + ref[j]
 
-func comb(ar []int, v []bool, s, n, r int, res *[]string) {
-	if r == 0 {
-		var m, h int
-		for i := 0; i < n; i++ {
-			if v[i] {
-				if ar[i] > 100 {
-					h += ar[i] - 100
-				} else {
-					m += ar[i]
-				}
+			if b != turnedOn {
+				continue
 			}
-		}
 
-		if m > 59 {
-			return
+			res = append(res, fmt.Sprintf("%d:%02d", i, j))
 		}
-		if h > 11 {
-			return
-		}
-		*res = append(*res, fmt.Sprintf("%d:%02d", h, m))
-		return
 	}
-
-	for i := s; i < n; i++ {
-		v[i] = true
-		comb(ar, v, i+1, n, r-1, res)
-		v[i] = false
-	}
+	return res
 }
