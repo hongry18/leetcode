9f66cfcc89a1d4192c65c9d34b0abdf7725613f1
hongry <hongry@hongryui-MacBookPro.local>
2023-01-23 13:21:20 +0900

    code optimize

--- go/easy/448.Find-All-Numbers-Disappeared-in-an-Array/main.go
+++ go/easy/448.Find-All-Numbers-Disappeared-in-an-Array/main.go
@@ -2,17 +2,18 @@ package main
 
 func findDisappearedNumbers(nums []int) []int {
 	var ar = make([]int, len(nums))
-	var res []int
 
 	for _, v := range nums {
 		ar[v-1]++
 	}
 
+	var j int
 	for i, v := range ar {
 		if v == 0 {
-			res = append(res, i+1)
+			ar[j] = i + 1
+			j++
 		}
 	}
 
-	return res
+	return ar[:j]
 }
