121d6df7282732a2f67162d1cbe0ddbe0f4ddf30
hongry <hongry@hongryui-MacBookPro.local>
2023-01-23 23:17:31 +0900

    memory optimize

--- go/easy/461.Hamming-Distance/main.go
+++ go/easy/461.Hamming-Distance/main.go
@@ -2,16 +2,12 @@ package main
 
 func hammingDistance(x int, y int) int {
 	var cnt int
-	if x < y {
-		x, y = y, x
-	}
-
+	x ^= y
 	for x > 0 {
-		if x&1 != y&1 {
+		if x&1 != 0 {
 			cnt++
 		}
 		x >>= 1
-		y >>= 1
 	}
 	return cnt
 }
