68034031c0b3311e1e1e27539d09c2e1a8cf5d5e
hongry <hongry@hongryui-MacBookPro.local>
2023-01-24 00:21:30 +0900

    code optimize

---
+++ go/easy/463.Island-Perimeter/main.go
@@ -0,0 +1,22 @@
+package main
+
+func islandPerimeter(grid [][]int) int {
+	var cnt int
+
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == 0 {
+				continue
+			}
+			cnt += 4
+			if i > 0 && grid[i-1][j] == 1 {
+				cnt -= 2
+			}
+			if j > 0 && grid[i][j-1] == 1 {
+				cnt -= 2
+			}
+		}
+	}
+
+	return cnt
+}
---
+++ go/easy/463.Island-Perimeter/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXxx(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		assert.Equal(t, islandPerimeter([][]int{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}), 16)
+	})
+}
