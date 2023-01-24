bafac2a6a007cfd8990541863599c6848b7d278d
hongry <hongry@hongryui-MacBookPro.local>
2023-01-24 10:52:11 +0900

    dfs

--- go/easy/463.Island-Perimeter/main.go
+++ go/easy/463.Island-Perimeter/main.go
@@ -1,20 +1,42 @@
 package main
 
+import "fmt"
+
 func islandPerimeter(grid [][]int) int {
 	var cnt int
 
-	for i := range grid {
-		for j := range grid[i] {
-			if grid[i][j] == 0 {
+	var ar [][]int
+	for gy := range grid {
+		for gx := range grid[gy] {
+			if grid[gy][gx] != 1 {
 				continue
 			}
-			cnt += 4
-			if i > 0 && grid[i-1][j] == 1 {
-				cnt -= 2
+			ar = append(ar, []int{gy, gx})
+
+			for len(ar) != 0 {
+				el := ar[len(ar)-1]
+				ar = ar[:len(ar)-1]
+				x, y := el[1], el[0]
+				if x < 0 || x >= len(grid[0]) || y < 0 || y >= len(grid) || grid[y][x] == 0 {
+					fmt.Println(y, x)
+					cnt++
+					continue
+				}
+
+				if grid[y][x] == -1 {
+					continue
+				}
+
+				grid[y][x] = -1
+
+				ar = append(ar,
+					[]int{y + 1, x},
+					[]int{y - 1, x},
+					[]int{y, x + 1},
+					[]int{y, x - 1},
+				)
 			}
-			if j > 0 && grid[i][j-1] == 1 {
-				cnt -= 2
-			}
+			break
 		}
 	}
 
