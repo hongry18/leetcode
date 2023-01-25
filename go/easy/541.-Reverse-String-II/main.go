7fb0664dcaf861f5ab7ea0bb760cd36143ec90a1
hongry <hongry@hongryui-MacBookPro.local>
2023-01-25 21:47:38 +0900

    code optimize

--- go/easy/541.-Reverse-String-II/main.go
+++ go/easy/541.-Reverse-String-II/main.go
@@ -3,14 +3,19 @@ package main
 func reverseStr(s string, k int) string {
 	var b = []byte(s)
 	for i := 0; i < len(b); i += k * 2 {
-		l := i + k
-		if l > len(b) {
-			l = len(b)
-		}
-		e := (l-i)/2 + i
-		for j := i; j < e; j++ {
-			b[j], b[l-1-j+i] = b[l-1-j+i], b[j]
+		if i+k < len(b) {
+			reverse(b[i : i+k])
+		} else {
+			reverse(b[i:])
 		}
 	}
 	return string(b)
+}
+
+func reverse(b []byte) {
+	var l, r = 0, len(b) - 1
+	for l < r {
+		b[l], b[r] = b[r], b[l]
+		l, r = l+1, r-1
+	}
 }
--- go/easy/541.-Reverse-String-II/main_test.go
+++ go/easy/541.-Reverse-String-II/main_test.go
@@ -9,5 +9,7 @@ import (
 func TestXxx(t *testing.T) {
 	t.Run("test1", func(t *testing.T) {
 		assert.Equal(t, reverseStr("abcdefghijklm", 5), "edcbafghijmlk")
+		assert.Equal(t, reverseStr("abcdefg", 2), "bacdfeg")
+		assert.Equal(t, reverseStr("abcd", 2), "bacd")
 	})
 }
