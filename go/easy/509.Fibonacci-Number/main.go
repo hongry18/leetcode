51dc4bdb06de8e5210b1c9e2220cd087d2d59d05
hongry <hongry@hongryui-MacBookPro.local>
2023-01-27 22:29:29 +0900

    optimize

--- go/easy/509.Fibonacci-Number/main.go
+++ go/easy/509.Fibonacci-Number/main.go
@@ -4,9 +4,11 @@ func fib(n int) int {
 	if n < 2 {
 		return n
 	}
-	var ar = []int{0, 1}
-	for i := 2; i <= n; i++ {
-		ar = append(ar, ar[i-1]+ar[i-2])
+
+	var a, b int = 0, 1
+	for i := 2; i < n; i++ {
+		a, b = b, b+a
 	}
-	return ar[n-1] + ar[n-2]
+
+	return a + b
 }
