0c4788538f2bd81865d148a5dfd47238698482d0
hongry <hongry@hongryui-MacBookPro.local>
2023-01-27 21:20:47 +0900

    code optimize

--- go/easy/520.Detect-Capital/main.go
+++ go/easy/520.Detect-Capital/main.go
@@ -1,30 +1,25 @@
 package main
 
 func detectCapitalUse(word string) bool {
-	if len(word) == 1 {
+	var nCap, nLow int
+	for _, b := range word {
+		if b >= 'a' && b <= 'z' {
+			nLow++
+		} else if b >= 'A' && b <= 'Z' {
+			nCap++
+		}
+	}
+
+	if nCap == 0 {
 		return true
 	}
-	var isUpper bool
-	if word[0] >= 'A' && word[0] <= 'Z' {
-		isUpper = true
+
+	if len(word) == nCap {
+		return true
 	}
-	for i := 1; i < len(word); i++ {
-		if !isUpper && word[i] >= 'A' && word[i] <= 'Z' {
-			return false
-		}
 
-		if i == 1 && isUpper && word[i] >= 'a' && word[i] <= 'z' {
-			isUpper = false
-			continue
-		}
-
-		if isUpper && (word[i] < 'A' || word[i] > 'Z') {
-			return false
-		}
-
-		if !isUpper && (word[i] < 'a' || word[i] > 'z') {
-			return false
-		}
+	if nCap == 1 && word[0] >= 'A' && word[0] <= 'Z' {
+		return true
 	}
-	return true
+	return false
 }
