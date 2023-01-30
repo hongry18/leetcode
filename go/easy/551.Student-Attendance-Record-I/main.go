123a679224e02e202466ee1beb535f3c8eb6ab59
hongry <hongry@hongryui-MacBookPro.local>
2023-01-30 23:12:18 +0900

    use util

--- go/easy/551.Student-Attendance-Record-I/main.go
+++ go/easy/551.Student-Attendance-Record-I/main.go
@@ -1,27 +1,7 @@
 package main
 
+import "strings"
+
 func checkRecord(s string) bool {
-	var a, l int
-
-	for i := range s {
-
-		if s[i] == 'L' {
-			l++
-			if l > 2 {
-				return false
-			}
-			continue
-		}
-
-		l = 0
-		if s[i] == 'P' {
-			continue
-		}
-		a++
-		if a > 1 {
-			return false
-		}
-	}
-
-	return true
+	return strings.Index(s, "LLL") == -1 && strings.Count(s, "A") < 2
 }
