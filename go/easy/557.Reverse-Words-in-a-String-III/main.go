ee24dda05ba0b4bc7397e35211885fdf7871afff
hongry <hongry@hongryui-MacBookPro.local>
2023-01-30 23:52:37 +0900

    ?? 이게 더 느리네

--- go/easy/557.Reverse-Words-in-a-String-III/main.go
+++ go/easy/557.Reverse-Words-in-a-String-III/main.go
@@ -1,25 +1,16 @@
 package main
 
 func reverseWords(s string) string {
-	var ar []int
-	ar = append(ar, 0)
+	var res, word []byte
 	for i := range s {
 		if s[i] == ' ' {
-			ar = append(ar, i)
-			ar = append(ar, i+1)
+			res = append(res, word...)
+			res = append(res, s[i])
+			word = []byte{}
+		} else {
+			word = append([]byte{s[i]}, word...)
 		}
 	}
-	ar = append(ar, len(s))
-
-	var b = []byte(s)
-
-	for i := 0; i < len(ar); i += 2 {
-
-		e := (ar[i+1] + ar[i])
-		for j := ar[i]; j < e/2; j++ {
-			b[j], b[e-1-j] = b[e-1-j], b[j]
-		}
-	}
-
-	return string(b)
+	res = append(res, word...)
+	return string(res)
 }
