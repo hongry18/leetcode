e8bc60e101a6359d8e122f8c1b4928cce895c61d
hongry <hongry@hongryui-MacBookPro.local>
2023-02-04 23:11:44 +0900

    solve

--- go/easy/589. N-ary Tree Preorder Traversal/main.go
+++ go/easy/589. N-ary Tree Preorder Traversal/main.go
@@ -9,16 +9,17 @@ func preorder(root *Node) []int {
 	if root == nil {
 		return []int{}
 	}
-
-	var ar = []*Node{root}
-	var res []int
-	for len(ar) != 0 {
-		n := ar[len(ar)-1]
-		ar = ar[:len(ar)-1]
-		res = append(res, n.Val)
-		for i := len(n.Children) - 1; i >= 0; i-- {
-			ar = append(ar, n.Children[i])
-		}
-	}
+	var res = []int{root.Val}
+	p(root, &res)
 	return res
+}
+
+func p(parent *Node, res *[]int) {
+	for _, n := range parent.Children {
+		if n == nil {
+			return
+		}
+		*res = append(*res, n.Val)
+		p(n, res)
+	}
 }
