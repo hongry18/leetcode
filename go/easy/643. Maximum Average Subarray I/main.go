28f551a9ce9be2ce891dca192edbde6cd238e55a
hongry <hongry@hongryui-MacBookPro.local>
2023-02-08 21:47:50 +0900

    solv

--- go/easy/643. Maximum Average Subarray I/main.go
+++ go/easy/643. Maximum Average Subarray I/main.go
@@ -1,21 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(findMaxAverage([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4))
+	// fmt.Println(findMaxAverage([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4))
+	fmt.Println(findMaxAverage([]int{-1}, 1))
 }
 func findMaxAverage(nums []int, k int) float64 {
-	var res float64
-	for i := 0; i < len(nums)-k+1; i++ {
-		var sum float64
-		for j := i; j < i+k; j++ {
-			sum += float64(nums[j])
-		}
-		t := sum / float64(k)
-		if res < t {
-			res = t
+	ans := math.MinInt
+	s := 0
+	for i, v := range nums {
+		s += v
+		if i >= k-1 {
+			if i != k-1 {
+				s = s - nums[i-k]
+			}
+			if ans < s {
+				ans = s
+			}
 		}
 	}
-	return res
+	return float64(ans) / float64(k)
 }
