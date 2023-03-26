ff76d275e7303fc3ba0c34e78b6f3e41d863a6e3
hongry <hongry@hongry-MacBook-Pro-14.local>
2023-03-26 18:36:48 +0900

    edit

--- go/easy/922. Sort Array By Parity II/main.go
+++ go/easy/922. Sort Array By Parity II/main.go
@@ -1,25 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	sortArrayByParityII([]int{4, 7, 5, 2})
+	sortArrayByParityII([]int{4, 2, 5, 7})
+	sortArrayByParityII([]int{2, 3})
+	sortArrayByParityII([]int{648, 831, 560, 986, 192, 424, 997, 829, 897, 843})
+	sortArrayByParityII([]int{8, 1, 60, 86, 92, 24, 7, 9, 7, 3})
 }
 
 func sortArrayByParityII(nums []int) []int {
-	var i, j = 0, len(nums) - 1
-
-	for i <= j {
-		if (i&1 == 0 && nums[i]&1 == 0) || (i&1 == 1 && nums[i]&1 == 1) {
-			i++
+	for i, num := range nums {
+		if (i&1 == 0 && num&1 == 0) || (i&1 == 1 && num&1 == 1) {
 			continue
 		}
 
-		if (j&1 == 0 && nums[j]&1 == 0) || (j&1 == 1 && nums[j]&1 == 1) {
-			j--
-			continue
+		for j := i + 1; j < len(nums); j++ {
+			if (i&1 == 0 && nums[j]&1 == 0) || (i&1 == 1 && nums[j]&1 == 1) {
+				nums[i], nums[j] = nums[j], nums[i]
+				break
+			}
 		}
-
-		nums[i], nums[j] = nums[j], nums[i]
-		i++
 	}
+	fmt.Println(nums)
 	return nums
 }
