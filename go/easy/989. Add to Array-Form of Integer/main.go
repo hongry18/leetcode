310dfd11416125d0f593170ade2e1f81085f5b9c
hongry <hongry@hongry-MacBook-Pro-14.local>
2023-04-02 11:42:09 +0900

    생각을 더 하면 좋을거같다

--- go/easy/989. Add to Array-Form of Integer/main.go
+++ go/easy/989. Add to Array-Form of Integer/main.go
@@ -1,27 +1,61 @@
 package main
 
+import "fmt"
+
 func main() {
 	addToArrayForm([]int{1, 2, 0, 0}, 34)
+	addToArrayForm([]int{2, 7, 4}, 181)
+	addToArrayForm([]int{2, 1, 5}, 806)
+	addToArrayForm([]int{1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3}, 516)
+	addToArrayForm([]int{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}, 1)
+	addToArrayForm([]int{0}, 23)
+	addToArrayForm([]int{6}, 809)
 }
 
 func addToArrayForm(num []int, k int) []int {
-	var res []int
-
-	var s int
-	for _, n := range num {
-		s = s*10 + n
+	ar := []int{}
+	for k > 0 {
+		ar = append(ar, k%10)
+		k /= 10
 	}
 
-	s += k
-
-	for s > 0 {
-		res = append(res, s%10)
-		s /= 10
+	for i, j := 0, len(ar)-1; i < j; i, j = i+1, j-1 {
+		ar[i], ar[j] = ar[j], ar[i]
 	}
 
-	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
-		res[i], res[j] = res[j], res[i]
+	if len(ar) > len(num) {
+		ar, num = num, ar
 	}
 
-	return res
+	var j int = len(num) - 1
+	var t int
+	for i := len(ar) - 1; i >= 0; i-- {
+		num[j] += t + ar[i]
+		if num[j] > 9 {
+			num[j] -= 10
+			t = 1
+		} else {
+			t = 0
+		}
+		j--
+	}
+
+	for j >= 0 {
+		num[j] += t
+		if num[j] > 9 {
+			num[j] -= 10
+			t = 1
+		} else {
+			t = 0
+			break
+		}
+		j--
+	}
+
+	if t == 1 {
+		num = append([]int{1}, num...)
+	}
+
+	fmt.Println(num)
+	return num
 }
